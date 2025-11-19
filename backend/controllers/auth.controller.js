c89fc4ad8664f463d722e2e07ae37acd14078431
Huzaifa <imsyed1998@gmail.com>
2025-11-19 16:19:14 +0500

    Initial commit with ecommerce store

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,23 @@
+import User from "../models/user.model.js";
+export const signup = async (req, res) => {
+    try {
+        const { email, password,name } = req.body;
+        console.log(req.body);
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: "User already exist" })
+        }
+        const user = await User.create({ name, email, password });
+        res.status(201).json({ user, message: "User created successfully!" });
+
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+export const login = async (req, res) => {
+    res.send("login route");
+}
+export const logout = async (req, res) => {
+    res.send("logout route");
+}
