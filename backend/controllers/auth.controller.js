8123bc70ea5d95a42b82c09bf41c493d82051ebb
Syed Huzaifa <50304066+SyedHuzaifa98@users.noreply.github.com>
2025-11-21 08:06:38 +0500

    -> redis setup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,21 @@
+import redis from '../lib/redis.js';
 import User from "../models/user.model.js";
+import { jwt } from "jsonwebtoken";
+
+
+const generateTokens = (userId) =>{
+    const accessToken = jwt.sign({userId},process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:"15m"
+    });
+    const refreshToken = jwt.sign({userId},process.env.REFRESH_TOKEN_SECRET,{
+        expiresIn:"7d"
+    });
+    return {accessToken,refreshToken};
+}
+
+const storeRefreshToken = async(userId,refreshToken) => {
+    await redis.set(`refresh_token: ${userId}`,refreshToken,"EX",7*24*60*60);
+}
 export const signup = async (req, res) => {
     try {
         const { email, password,name } = req.body;
@@ -8,6 +25,9 @@ export const signup = async (req, res) => {
             return res.status(400).json({ message: "User already exist" })
         }
         const user = await User.create({ name, email, password });
+        const {accessToken,refreshToken} = generateTokens(user._id,refreshToken);
+        
+        await storeRefreshToken(user._id)
         res.status(201).json({ user, message: "User created successfully!" });
 
     }
